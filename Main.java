46f818294090824f9b57c27f6deaa8c77259279c
Sapriadi <147253388+Sapriadiadi@users.noreply.github.com>
2023-10-09 21:18:52 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+package sapriadi;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama = "sapriadi" ;
+      int umur = 18;
+      long tahunLahir = 2005;
+      short tanggalLahir = 5 ;
+      byte bulanLahir = 6 ;
+      double tinggiBadan = 169.5;
+      float beratBadan = 55.0f ;
+      char ukuranBaju = 'L' ;
+      boolean statusMenikah = false;
+      
+        System.out.println("nama :" + nama);
+        System.out.println("umur :" + umur);
+        System.out.println("tahun lahir :" + tahunLahir);
+        System.out.println("tanggal lahir :" + tanggalLahir);
+        System.out.println("bulan lahir :" + bulanLahir);
+        System.out.println("tinggi badan :" + tinggiBadan);
+        System.out.println("berat badan :" + beratBadan);
+        System.out.println("ukuran baju ;" + ukuranBaju);
+        System.out.println("status menikah :" + statusMenikah);
+    
+    }
+    
+}

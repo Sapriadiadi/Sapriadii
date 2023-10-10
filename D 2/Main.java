b37be9e7abe4f3ce3fe33cc70828805284e1c0c6
Sapriadi <147253388+Sapriadiadi@users.noreply.github.com>
2023-10-10 21:29:57 +0800

    Add files via upload
    Karena di kalikan baru di jumlahkan seperti 12x3=36 baru di jumlahkan 10 jadi hasilnya 46

---
+++ D 2/Main.java
@@ -0,0 +1,19 @@
+package adiii;
+
+public class Main {
+    
+    public static void main(String[] args) {
+    int angkaPertama = 10;
+        int angkaKedua = 3;
+        int angkaketiga = 12;
+        int hasil = +angkaPertama + angkaKedua * angkaketiga ;
+        
+
+        System.out.println("Hasil penambahan: " + hasil);
+
+      
+
+
+    }
+    
+}

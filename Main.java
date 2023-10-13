3166374fa8ac3c98958a1c2887cb2b53b6c5bb8a
Sapriadi <147253388+Sapriadiadi@users.noreply.github.com>
2023-10-13 20:53:32 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,28 +1,13 @@
-package sapriadi;
+package yt;
 
 public class Main {
     
     public static void main(String[] args) {
-        String nama = "sapriadi" ;
-      int umur = 18;
-      long tahunLahir = 2005;
-      short tanggalLahir = 5 ;
-      byte bulanLahir = 6 ;
-      double tinggiBadan = 169.5;
-      float beratBadan = 55.0f ;
-      char ukuranBaju = 'L' ;
-      boolean statusMenikah = false;
-      
-        System.out.println("nama :" + nama);
-        System.out.println("umur :" + umur);
-        System.out.println("tahun lahir :" + tahunLahir);
-        System.out.println("tanggal lahir :" + tanggalLahir);
-        System.out.println("bulan lahir :" + bulanLahir);
-        System.out.println("tinggi badan :" + tinggiBadan);
-        System.out.println("berat badan :" + beratBadan);
-        System.out.println("ukuran baju ;" + ukuranBaju);
-        System.out.println("status menikah :" + statusMenikah);
-    
+        
+        int nilai = 2 ;
+        int angka = 3;
+        System.out.println(angka==nilai || angka!=nilai);
+        System.out.print(angka==nilai && angka!=nilai);
     }
     
 }
